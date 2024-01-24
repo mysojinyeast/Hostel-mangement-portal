3ecbbb8bfbb5f22f8383efd83c819bdbd9e809d6
mysojinyeast <21cs3003@rgipt.ac.in>
2024-01-24 18:54:01 +0530

    initial commit

---
+++ users/includes/header.php
@@ -0,0 +1,13 @@
+ <header class="header black-bg">
+              <div class="sidebar-toggle-box">
+                  <div class="fa fa-bars tooltips" data-placement="right" data-original-title="Toggle Navigation"></div>
+              </div>
+            
+            <a href="https://www.rgipt.ac.in/" class="logo"><b>RGIPT Complaint Management System</b></a>
+                
+            <div class="top-menu">
+            	<ul class="nav pull-right top-menu">
+                    <li><a class="logout" href="logout.php">Logout</a></li>
+            	</ul>
+            </div>
+        </header>
